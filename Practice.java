1cc08122be605d0b5d1d151a339767404628bccc
KaVin S <kavinadithya3@gmail.com>
2024-03-15 22:31:08 +0530

    Input From the User

--- Practice.java
+++ Practice.java
@@ -6,6 +6,8 @@ class Main{
         System.out.println("Instance Block Loaded!!!");
     }
     public static void main(String []args) throws Exception{
+        @SuppressWarnings("deprecation")
         Main obj=(Main)Class.forName("Main").newInstance();
+        obj.hashCode();
     }
 }
