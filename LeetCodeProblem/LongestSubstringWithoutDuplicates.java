dc1f9e038df839f2049de26782c27bf84cb4ceb3
KaVin S <kavinadithya3@gmail.com>
2024-03-21 23:08:11 +0530

    Longest SubString

--- LeetCodeProblem/LongestSubstringWithoutDuplicates.java
+++ LeetCodeProblem/LongestSubstringWithoutDuplicates.java
@@ -8,19 +8,24 @@ public class LongestSubstringWithoutDuplicates {
         for(int right=0;right<s.length();right++){
             if(!set.contains(s.charAt(right))){
                 set.add(s.charAt(right));
-                maxLength=Math.max(maxLength,right-left+1);
+                //maxLength=Math.max(maxLength,right-left+1);
             }
             else{
-                while(set.contains(s.charAt(right))){
-                    set.remove(s.charAt(left));
-                    left++;
-                }
+//                while(set.contains(s.charAt(right))){
+//                    set.remove(s.charAt(left));
+//                    left++;
+//                }
+                maxLength=Math.max(set.size(),maxLength);
+                left=right;
+                set.clear();
                 set.add(s.charAt(right));
+
             }
         }
+        maxLength=Math.max(set.size(),maxLength);
         return maxLength;
     }
     public static void main(String[] args){
-        System.out.println(findLongestWord("abcabcbb"));
+        System.out.println(findLongestWord("pwwkew"));
     }
 }
