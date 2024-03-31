eb3afe59f37934d5e74c0e0c45d9a855d544ec3d
KaVin S <kavinadithya3@gmail.com>
2024-03-31 07:41:14 +0530

    System.arraycopy static method practice

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 class WorkSpace{
     public static void main(String []args){
+
         System.out.println("Kavin\nNew Era Will Begin Soon Techcrack");
+        int[] nums={1,2,3,9,8,0};
+        int[] nums1={1,2,4,5,6,5,6};
+        System.arraycopy(nums1,2,nums,3,3);
+        for(int n:nums){
+            System.out.println(n);
+        }
     }
 }
