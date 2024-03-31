3161469553d8e4892bd950f3e77534280821d303
KaVin S <kavinadithya3@gmail.com>
2024-03-31 10:01:15 +0530

    String Palindrome Where string is alpha numeric and coonverting the string into  lower  case

--- LeetCodeProblem/BinaryAddittionTest.java
+++ LeetCodeProblem/BinaryAddittionTest.java
@@ -29,4 +29,9 @@ class BinaryAddittionTest {
         BinaryAddittion ba=new BinaryAddittion();
         assertEquals("11101110000",ba.binaryAdd("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101","110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
     }
+    @Test
+    public void testString(){
+        StringPalindrome sp=new StringPalindrome();
+        assertTrue(sp.checkThePalindrome("Ka viv! Ak"));
+    }
 }
