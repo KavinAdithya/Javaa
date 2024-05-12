dd2aa4c94664e0133e54782a5d4c975ee92ad8cb
KaVin S <kavinadithya3@gmail.com>
2024-05-12 09:36:24 +0530

    Learned About the PriorityQueue concepts

---
+++ src/main/java/com/techcrack/OddOrEven.java
@@ -0,0 +1,20 @@
+package com.techcrack;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    //We needed to find the given number is odd or even
+    public static void main(String[] args){
+        //Creating object for scanner class which is used to get input from user on console
+        Scanner scan=new Scanner(System.in);
+
+        int n=Integer.parseInt(scan.nextLine());
+
+        if(n%2==0)
+            System.out.println(n+" is Even.");
+        else
+            System.out.println(n+" is odd.");
+
+    }
+}
---
+++ src/main/java/com/techcrack/Student.java
@@ -0,0 +1,73 @@
+package com.techcrack;
+
+import java.util.Arrays;
+
+public class Student implements Comparable<Student>{
+
+
+    private int semesterMarkUpdated=0;
+    private String name;
+    private int age;
+    private final double[] cgpa=new double[8];
+
+    private double sgp;
+
+    public Student(String name, int age, double sgp) {
+        this.name = name;
+        this.age = age;
+        this.sgp = sgp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double[] getCgpa() {
+        return cgpa;
+    }
+
+    public double getSgp() {
+        return sgp;
+    }
+
+    public void setSgp(double sgp) {
+        this.sgp = sgp;
+    }
+
+
+    public void addCgpa(int n){
+        if(semesterMarkUpdated==cgpa.length)
+            System.out.println("All Semester Marks Has been Updated Already");
+        else
+            cgpa[semesterMarkUpdated++]=n;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", cgpa=" + Arrays.toString(cgpa) +
+                ", sgpa=" + sgp +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(o.sgp==this.sgp)
+            return 0;
+        return this.sgp>o.sgp?1:-1;
+    }
+}
