ddb815a7ddc00468bb418d41c26fc4fa719e276b
KaVin S <kavinadithya3@gmail.com>
2024-07-29 18:54:55 +0530

    Learned About the wildcard upper bounds

--- src/main/java/com/techcrack/Generics/MaximumWildCard.java
+++ src/main/java/com/techcrack/Generics/MaximumWildCard.java
@@ -8,7 +8,7 @@ public class MaximumWildCard {
     // Finding the maximum element.
     // Element types are not restricted.
     public static <T extends Comparable<? super T>> T findMax(List<T> list) {
-        // Check for null or empty list
+        // Check for a null or empty list
         if (list == null || list.isEmpty()) {
             return null;
         }
@@ -18,13 +18,28 @@ public class MaximumWildCard {
 
         // Iterate over the list to find the maximum element
         for (T element : list) {
-
-
+            // Comparing the Objects Based on the elements of generic types
             if (element.compareTo(maxElement) > 0) {
                 maxElement = element;
             }
         }
 
+        // return the maximum element in a list
         return maxElement;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(89, 1, 67, 23, 45);
+
+        System.out.println("Maximum Element in a list is : " + findMax(list));
+
+        List<Double> lisOfDouble = Arrays.asList(1.2, 1.202, 100.0, 1.2922, 100.09);
+
+        System.out.println("Maximum Element in a list is : " + findMax(lisOfDouble));
+
+        List<String> listOfString = Arrays.asList("Kavin", "KavinDharani", "Dharani");
+
+        System.out.println("Maximum Element in a array is : " + findMax(listOfString));
+
     }
 }
---
+++ src/main/java/com/techcrack/Generics/ReadOnlySum.java
@@ -0,0 +1,35 @@
+package com.techcrack.Generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// This Class is used to Demonstrate the functionality of Read-only of wildcards and
+// We Needed to sum it.
+public class ReadOnlySum {
+
+    // @param List is contained its element is an unknown type but its wildcard
+    // It ensures the type safety and restricting the list to read-only
+    // Returns sum of the list elements
+    static double sumNumbers(List<? extends Number> list) {
+        // Storing the Sum Values Here
+        double sum = 0;
+
+        // Iterating the every element in a list
+        for (Number number : list)
+            sum += number.doubleValue();
+
+        // returning the sum value
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        List<Double> listOfDouble = Arrays.asList(1.1, 1.2, 1.3, 1.4, 1.5);
+
+        System.out.println("Sum of Integer Value is : " + sumNumbers(list));
+
+        System.out.println("Sum of Double Value is : " + sumNumbers(listOfDouble));
+    }
+
+}
