e88aec5661b677a9b502b366cb20bda735f53bff
Kavin <kavinadithya3@gmail.com>
2024-08-29 14:32:36 +0530

    Sieve of Eratosthenes to find prime numbers between range

---
+++ src/main/java/com/techcrack/Algorithms/PrimeNumberRange.java
@@ -0,0 +1,32 @@
+// import java.util.Arrays;
+
+public class PrimeNumberRange {
+    static boolean[] sieveOfEratosthenes(int n) {
+        boolean[] isPrime = new boolean[n];
+
+        isPrime[0] = true;
+        isPrime[1] = true;
+
+        for (int i = 2; i * i < n; i++) {
+
+            if (! isPrime[i]) {
+                for (int j = i  * i; j < n; j += i)
+                    isPrime[j] = true;
+            }
+        }
+
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        int n = 50;
+        boolean[] isPrime = sieveOfEratosthenes(n + 1);
+
+        //System.out.prinztln(Arrays.toString(isPrime));
+
+        for (int i = 0; i < n; i++) {
+            if ( ! isPrime[i])
+                System.out.print(i + " ");
+        }
+    }
+}
